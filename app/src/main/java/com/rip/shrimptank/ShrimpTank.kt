b31e838d49b02caa3e1958d8045a4bef818e32e9
Ohad Ripshtos <105322785+Ripshtos@users.noreply.github.com>
2025-03-19 01:29:46 +0200

    added cloudinairy integration

--- app/src/main/java/com/rip/shrimptank/ShrimpTank.kt
+++ app/src/main/java/com/rip/shrimptank/ShrimpTank.kt
@@ -2,10 +2,15 @@ package com.rip.shrimptank
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import android.content.Context
 
 @HiltAndroidApp
 class ShrimpTank : Application() {
+    object Globals {
+        var context: Context? = null
+    }
     override fun onCreate() {
         super.onCreate()
+        Globals.context = applicationContext
     }
 }
