31b14e653e27dc847a94944eb91d9aa2f1276eb8
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-03-29 12:53:08 +0300

    Fix Top Bar

--- app/src/main/java/com/rip/shrimptank/ui/MainActivity.kt
+++ app/src/main/java/com/rip/shrimptank/ui/MainActivity.kt
@@ -1,29 +1,71 @@
 package com.rip.shrimptank.ui
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.rip.shrimptank.R
+import com.rip.shrimptank.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FragmentChangeListener {
+
+    private lateinit var context: Context
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bottomMenu()
+        context = this
+
+        setUpToolbar()
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController, AppBarConfiguration(navController.graph))
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(destination.id != R.id.homeFragment &&
+                        destination.id != R.id.myPosts &&
+                        destination.id != R.id.search &&
+                        destination.id != R.id.explore &&
+                        destination.id != R.id.profile)
+                setHomeAsUpIndicator(R.drawable.ic_back_arrow)
+            }
+        }
     }
 
-    private fun bottomMenu() {
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation_view)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    private fun setUpToolbar(){
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.apply {
+            setTitleTextColor(ContextCompat.getColor(context,R.color.white))
+        }
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun navigateToFrag(fragmentId: Int, popUpId: Int) {
+        navController.navigate(fragmentId)
+    }
+}
+
+interface FragmentChangeListener {
+    fun navigateToFrag(fragmentId: Int,popUpId:Int=0)
 }
